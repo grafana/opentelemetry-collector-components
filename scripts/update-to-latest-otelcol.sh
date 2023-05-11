bb4f014d5f2f072b4965d5b9dad90c25be1186f0
Juraci Paixão Kröhling <juraci@kroehling.de>
2023-05-11 15:16:41 -0300

    Bump OpenTelemetry core and/or contrib versions
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- scripts/update-to-latest-otelcol.sh
+++ scripts/update-to-latest-otelcol.sh
@@ -84,6 +84,7 @@ for gomod in $gomods; do
     pushd "$(dirname $gomod)"
     sed -i "s~\(go\.opentelemetry\.io/collector.*\s\).*\$~\1v${latest_core_version}~" $gomod
     sed -i "s~\(.*github.com/open-telemetry/opentelemetry-collector-contrib/.*\s\).*~\1v${latest_contrib_version}~" $gomod
+    go mod tidy
     popd
 done
 
